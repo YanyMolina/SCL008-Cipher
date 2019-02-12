edb931665476aa68347059738397202adac9105c
YanyMolina <yaniza.molina@gmail.com>
2019-02-12 18:41:15 -0300

    Proyecto terminado sin readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,33 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+   
+   encode: (offset, codetext) => {
+
+    let word_text = '';
+ 
+    for (let i=0; i < codetext.length; i++){
+        let asciiCode = codetext.charCodeAt(i);
+        let newAsciiCode = "";
+ 
+        if (asciiCode >= 65 && asciiCode <= 90){
+         newAsciiCode = (asciiCode - 65 + parseInt (offset)) % 26 + 65;
+        }
+     word_text +=  String.fromCharCode(newAsciiCode);
+    }
+ 
+    return word_text;
+ },
+  decode:(offset,text) => {
+ 
+    let deco_text = '';
+ 
+    for (let i = 0; i < text.length; i++) {
+ 
+        let asciiCode = text.charCodeAt(i);
+ if (asciiCode >= 65 && asciiCode <= 90) {
+            asciiCode = ((asciiCode - 90 - offset) % 26) + 90;
+    }
+    deco_text = deco_text + String.fromCharCode(asciiCode);
+ }
+    return deco_text;
+ }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-/* Acá va tu código */
+  
+  window.onload = () => {
+  //Mostrar pantalla de cifrado y ocultar home
+  const button_enter = document.getElementById('btningresar');
+
+  const welcome = document.getElementById('welcome');
+  const cipher = document.getElementById('cipher');
+
+  button_enter.addEventListener('click', () => {
+    welcome.style.display ="none";
+    cipher.style.display="block";
+  });
+  
+  const buttonencode = document.getElementById('btnencode');
+
+   buttonencode.addEventListener('click', () => {
+    
+   let codetext = document.getElementById ('code').value;
+   let offset = document.getElementById ('offset').value; 
+   let encoderesult = window.cipher.encode (offset, codetext);
+  document.getElementById('contenedor_datos').innerHTML = (encoderesult);
+
+  })
+
+  const buttondecode = document.getElementById('btndecode');
+   buttondecode.addEventListener('click', () => {
+
+  let text = document.getElementById ('code').value;
+  let offset = document.getElementById ('offset').value; 
+  let decoderesult = window.cipher.decode (offset, text);
+  document.getElementById('contenedor_datos').innerHTML = (decoderesult);
+
+
+  })
+}
